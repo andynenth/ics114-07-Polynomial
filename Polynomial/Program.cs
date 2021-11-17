876ae7fbd5d7fd329a10fcb2c64834cef9e4f7f9
Andy Nenthong <C0516154@intra.camosun.bc.ca>
2021-11-17 14:21:30 -0800

    Add project files.

---
+++ Polynomial/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Polynomial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] p1 = {8, -4, 5, 2};
+            int[] p2 = new int[p1.Length];
+            
+            Console.Write(" Enter a nember: ");
+            string inVal = Console.ReadLine();
+
+            int number;
+            while (!int.TryParse(inVal, out number))
+            {
+                Console.WriteLine(" Invalid number \"{0\" entered. Please re-enter a number.", inVal);
+            }
+            
+            Console.WriteLine("Original array:");
+            
+            for (int i = 0; i < p1.Length; i++)
+            {
+                Console.Write(" {0}",p1[i]);
+                p2[i] = p1[i] * number;
+            }
+            
+            Console.Write("\n\n");
+            Console.WriteLine("Modified array:");
+
+            foreach (var literalValue in p2)
+            {
+                Console.Write(" {0}", literalValue);
+            }
+            
+            Console.WriteLine();
+        }
+    }
+}
